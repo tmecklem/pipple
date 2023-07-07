156bfe0c3b923b12bfb2333e010f022d7b6a6792
Tim Mecklem <timothy@mecklem.com>
2023-07-07 10:35:40 -0400

    Add preferred names to struct

--- lib/pipple/employee.ex
+++ lib/pipple/employee.ex
@@ -6,46 +6,52 @@ defmodule Pipple.Employee do
   use TypedStruct
 
   typedstruct do
-    field(:id, String.t())
-    field(:name, String.t())
-    field(:last_name, String.t())
-    field(:first_name, String.t())
-    field(:title, String.t())
-    field(:department, String.t())
-    field(:custom_fields, list())
-    field(:updated_at, DateTime.t())
     field(:created_at, DateTime.t())
-    field(:start_date, Date.t())
+    field(:custom_fields, list())
+    field(:department, String.t())
+    field(:first_name, String.t())
+    field(:id, String.t())
+    field(:last_name, String.t())
     field(:manager, String.t())
+    field(:name, String.t())
+    field(:preferred_first_name, String.t())
+    field(:preferred_last_name, String.t())
+    field(:start_date, Date.t())
+    field(:title, String.t())
+    field(:updated_at, DateTime.t())
   end
 
-  def new(%{
-        "id" => id,
-        "name" => name,
-        "firstName" => first_name,
-        "lastName" => last_name,
-        "title" => title,
-        "department" => department,
-        "customFields" => custom_fields,
-        "updatedAt" => updated_at,
-        "createdAt" => created_at,
-        "startDate" => start_date,
-        "manager" => manager
-      }) do
+  def new(
+        %{
+          "createdAt" => created_at,
+          "customFields" => custom_fields,
+          "department" => department,
+          "firstName" => first_name,
+          "id" => id,
+          "lastName" => last_name,
+          "manager" => manager,
+          "name" => name,
+          "startDate" => start_date,
+          "title" => title,
+          "updatedAt" => updated_at
+        } = response
+      ) do
     struct(
       __MODULE__,
       %{
-        id: id,
-        name: name,
-        first_name: first_name,
-        last_name: last_name,
-        title: title,
-        department: department,
-        custom_fields: custom_fields,
-        updated_at: Timex.parse!(updated_at, "{ISO:Extended}"),
         created_at: Timex.parse!(created_at, "{ISO:Extended}"),
+        custom_fields: custom_fields,
+        department: department,
+        first_name: first_name,
+        id: id,
+        last_name: last_name,
+        manager: manager,
+        name: name,
+        preferred_first_name: Map.get(response, "preferredFirstName", nil),
+        preferred_last_name: Map.get(response, "preferredLastName", nil),
         start_date: Timex.parse!(start_date, "%Y-%m-%d", :strftime),
-        manager: manager
+        title: title,
+        updated_at: Timex.parse!(updated_at, "{ISO:Extended}")
       }
     )
   end
